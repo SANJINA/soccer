e3f6b0f202b805dc309c50fc190803a8d4a3e0a1
Diwash Shrestha <dshrestha@techdigitalcorp.com>
2016-12-15 11:38:43 -0600

    Added Soccer Component

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,9 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 
+import { QuoteComponent } from './components/shared/quote/quote.component';
+
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [
+    AppComponent,
+    QuoteComponent,
+    HeaderComponent,
+    FooterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ app/components/shared/quote/quote.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'quote',
+  templateUrl: 'quote.component.html',
+})
+export class QuoteComponent  {}
