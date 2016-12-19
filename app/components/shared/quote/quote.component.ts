bfd337d916f4476b669cf3b0e20e0168ae957d7e
Diwash Shrestha <dshrestha@techdigitalcorp.com>
2016-12-19 09:29:32 -0600

    Added button for new quote in quote component

--- app/components/shared/quote/quote.component.ts
+++ app/components/shared/quote/quote.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 import { QuoteService } from '../../../services/quote/quote.services';
 
@@ -12,14 +12,39 @@ export class QuoteComponent implements OnInit {
   quote: string;
   quoteAuthor: string;
   errorMessage: string;
+  showNewQuoteButton: boolean;
 
-  constructor(private _quoteService: QuoteService) {}
+  constructor(private _quoteService: QuoteService) {
+    this.showNewQuoteButton = false;
+  }
 
   ngOnInit(): void {
+    this.getNewQuote();
+  }
+
+  /*
+  @HostListener('mouseenter') onMouseEnter() {
+    this.showNewQuoteBtn = true;
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.showNewQuoteBtn = false;
+  }
+  */
+
+  getNewQuote(): void {
     this._quoteService.getQuote().subscribe(res => {
       //console.log('What we got from service', res);
       this.quote = res.quoteText;
       this.quoteAuthor = res.quoteAuthor;
     });
+  }
+
+  showNewQuoteBtn(): void {
+    this.showNewQuoteButton = true;
+  }
+
+  hideNewQuoteBtn(): void {
+    this.showNewQuoteButton = false;
   }
 }
